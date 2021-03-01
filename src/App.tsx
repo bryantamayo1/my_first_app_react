986d3eec58a33b86ae371477ad13d327e658d137
bryantamayo1 <david_bryan34@hotmail.com>
2021-03-01 20:33:44 +0000

    Update background

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {tasks} from "./task.json";
@@ -15,26 +15,28 @@ class App extends React.Component<any, any >{
   render(){
     const tasksHTML = this.state.tasks.map( (task:any, i: number) => {
       return (
+        <div className="col-md-3">
             <div className="card">
-                {task.title}
+                
                 <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                  <h5 className="card-title">{task.title}</h5>
+                  <p className="card-text">{task.description}</p>
+                  <a href="#" className="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
+        </div>
       )
     });
 
     return (
       <div className="App">
-          <header className="header">
-            Task
-            <div>0</div>
-          </header>
-          {tasksHTML}
-          <img src={logo} className="App-logo" alt="logo"/>
-      </div>
+        <header className="header">
+                Task
+                <div>0</div>
+        </header>
+        {tasksHTML}
+        </div>
+
     );
   }
 }
