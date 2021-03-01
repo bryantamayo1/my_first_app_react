dabf85c0c34f45c666c95b262fa415040e184378
bryantamayo1 <david_bryan34@hotmail.com>
2021-03-01 19:00:32 +0000

    Update component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {tasks} from "./task.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component<any, any >{
+  constructor(props: any){
+    super(props);
+    this.state = {
+      tasks
+    }
+  }
+
+  render(){
+    const tasksHTML = this.state.tasks.map( (task:any, i: number) => {
+      return (
+            <div className="card">
+                {task.title}
+                <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+      )
+    });
+
+    return (
+      <div className="App">
+          <header className="header">
+            Task
+            <div>0</div>
+          </header>
+          {tasksHTML}
+          <img src={logo} className="App-logo" alt="logo"/>
+      </div>
+    );
+  }
 }
+
 
 export default App;
